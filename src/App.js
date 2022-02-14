e54c661c2ac6c2c5ab895ffba8bd957276189d4c
Bernardo Paes <bersantos22@gmail.com>
2022-02-14 18:08:45 -0300

    Ajustes na Main- css

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import './App.css';
 import { ErrorPage } from "./Pages/ErrorPage/ErrorPage";
 import { NavBar } from './Components/NavBar/NavBar';
 import 'bulma/css/bulma.css';
-import {Section1} from './Components/Section1/section1'
+/* import { HomePage } from "./Pages/HomePage/HomePage"; */
 
 function App() {
   return (
     <div className="App">
-     <h1>MY TOP 5 !</h1>
 
      <BrowserRouter>
-     
+     <NavBar />
      <Routes>
        <Route path="*" element={<ErrorPage/>}/>
      </Routes>
      </BrowserRouter>
-     
-     <NavBar />
-     <Section1 />
    
     </div>
   );
