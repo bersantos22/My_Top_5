6fe763a5a7519b742657fa3543cc8fbb3bd03eca
Bernardo Paes <bersantos22@gmail.com>
2022-02-15 09:44:45 -0300

    criando cards

---
+++ src/Components/Card/Cards.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css'
+
+export function Card(){
+    return(
+        <>
+            <div className='card-container'>
+                <div className='card-base card-one'>
+
+                </div>
+                <div className='card-base card-tow'>
+
+                </div>
+                <div className='card-base card-three'>
+
+                </div>
+                <div className='card-base card-four'>
+
+                </div>
+                <div className='card-base card-five'>
+
+                </div>
+            </div>
+        </>
+    )
+}
