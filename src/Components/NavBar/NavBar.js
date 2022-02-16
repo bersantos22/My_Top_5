ff19a311dff42003a4bcf3a19b87b611b08a5f30
Bernardo Paes <bersantos22@gmail.com>
2022-02-16 09:35:51 -0300

    arrumando a nav bar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -9,46 +9,42 @@ export function NavBar(){
                 <h2>MyTop 5</h2>
                 <div className='linksPage'>
                         <ul className='listUl'>
+
                             <li className='listLi'>
-
                             
-                            <Link className="nav-link text-light" to="/">
-                                Home
+                                <Link 
+                                    className="nav-link text-light" 
+                                    to="/"
+                                    >Home
                                 </Link>
-
-
-                            <Link 
-                            className='Link-Nav'
-                            to='/'
-                            >Home</Link>
 
                             </li>
                             <li className='listLi'>
                             
-                                <Link className="nav-link text-light" to="/post-top-5">
-                                Create Top 5
+                                <Link 
+                                    className="nav-link text-light" to="/post-top-5">
+                                    Create Top 5
                                 </Link>
                             </li>
+
                             <li className='listLi'>
 
-                                <Link lassName="nav-link text-light" to="/aboutus">
-                                About Us
+                                <Link 
+                                    className="nav-link text-light" 
+                                    to="/alltop5">
+                                    All Top 5
                                 </Link>
-                            
-                                {/* LINK AQUI */}
-
-
-                            <Link className='Link-Nav' to="/aboutUs">About Us</Link>
                             
 
                             </li>
 
                             <li className='listLi'>
-                                <Link lassName="nav-link text-light" to="/alltop5">
-                                All Top 5
+
+                                <Link 
+                                    className='Link-Nav' 
+                                    to="/aboutUs">About The Project
                                 </Link>
                             
-                               
                             </li>
                         </ul>
                 </div>
