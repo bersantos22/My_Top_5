fc93de6e15582b5eb14df56ca548c81474e5c796
Lenine Xavier <leninecxavier@gmail.com>
2022-02-17 15:12:48 -0300

    cards fixed

--- src/Pages/Top5/index.js
+++ src/Pages/Top5/index.js
@@ -15,55 +15,54 @@ export function Top5(){
                 const result = await axios.get(
                     `https://ironrest.herokuapp.com/mytop5/${id}`
                 );
-                console.log("***",result.data)
-                setTop5([...result.data]);
+                
+                setTop5({...result.data});
             }catch(err){
                 console.error(err);
             }
         }
 
         fetchTop5();
-        setNewrender(false);
+        //setNewrender(false);
 
-    },[newrender]);
+    },[]);
 
     return(
 
         <>
         
-        {top5.map((currentTop5)=>{
-            return(
-                <Card
-                id={currentTop5._id}
-                key={currentTop5._id}
+        
+               <Card
+                id={top5._id}
+                key={top5._id}
 
-                name={currentTop5.name}
+                name={top5.name}
 
-                name_m={currentTop5.name_m}
-                image_m={currentTop5.image_m}
-                description_m={currentTop5.description_m}
+                name_m={top5.name_m}
+                image_m={top5.image_m}
+                description_m={top5.description_m}
 
-                name_s={currentTop5.name_s}
-                image_s={currentTop5.image_s}
-                description_s={currentTop5.description_s}
+                name_s={top5.name_s}
+                image_s={top5.image_s}
+                description_s={top5.description_s}
 
-                name_b={currentTop5.name_b}
-                image_b={currentTop5.image_b}
-                description_b={currentTop5.description_b}
+                name_b={top5.name_b}
+                image_b={top5.image_b}
+                description_b={top5.description_b}
 
-                name_d={currentTop5.name_d}
-                image_d={currentTop5.image_d}
-                description_d={currentTop5.description_d}
+                name_d={top5.name_d}
+                image_d={top5.image_d}
+                description_d={top5.description_d}
                 
-                name_f={currentTop5.name_f}
-                image_f={currentTop5.image_f}
-                description_f={currentTop5.description_f}
+                name_f={top5.name_f}
+                image_f={top5.image_f}
+                description_f={top5.description_f}
 
                 
                 
                 />
-            )
-        })}
+           
+     
 
         
         </>
